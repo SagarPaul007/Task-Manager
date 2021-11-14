174151a086ed2fa56efda309a55beddf22cd3f62
Sagar <sagarpaul2k16@gmail.com>
2021-11-14 13:37:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import AddTask from "./components/AddTask/AddTask";
+import Tasks from "./components/Tasks/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="app">Task Manager</div>
+      <AddTask tasks={tasks} setTasks={setTasks} />
+      <Tasks tasks={tasks} setTasks={setTasks} />
     </div>
   );
 }
